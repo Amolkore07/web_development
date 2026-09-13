325e4082a44b76e7fd66a796a710189c62878872
Amol kote <koreamol710@gmail.com>
2026-09-14 01:04:33 +0530

    Arrays

---
+++ JAVA_SCRIPT/Array/Nested_array.js
@@ -0,0 +1,2 @@
+var arraynam =[["rohit", 11, 99.0] ,["sachiya", 9, 99.9]]
+console.log(arraynam)
---
+++ JAVA_SCRIPT/Array/multidimen_array.js
@@ -0,0 +1,5 @@
+var myarray = [[1,2,5,6], [5,4,2,6],[7,7,7,7]];
+
+var data = myarray[2][1];
+var data1 =myarray[1][2]; 
+console.log(data, data1)
